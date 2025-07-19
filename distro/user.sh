b3382f5264704a92f8cca23b3f833dc869d56055
DinhQuangDoi <dinhquangdoi99@gmail.com>
2025-07-20 01:08:17 +0700

    Update user.sh
    Vietsub

--- distro/user.sh
+++ distro/user.sh
@@ -12,25 +12,25 @@ banner() {
     printf "\033[36m    |  | |__] |  | |\ |  |  |  |    |\/| |  | |  \ \033[0m\n"
     printf "\033[32m    |__| |__] |__| | \|  |  |__|    |  | |__| |__/ \033[0m\n"
     printf "\033[0m\n"
-    printf "     \033[32mA modded gui version of ubuntu for Termux\033[0m\n"
+    printf "     \033[32mUbuntu Termux\033[0m\n"
     printf "\033[0m\n"
 
 }
 
 sudo() {
-    echo -e "\n${R} [${W}-${R}]${C} Installing Sudo..."${W}
+    echo -e "\n${R} [${W}-${R}]${C} Đang cài đặt Sudo..."${W}
     apt update -y
     apt install sudo -y
     apt install wget apt-utils locales-all dialog tzdata -y
-    echo -e "\n${R} [${W}-${R}]${G} Sudo Successfully Installed !"${W}
+    echo -e "\n${R} [${W}-${R}]${G} Sudo đã cài đặt thành công !"${W}
 
 }
 
 login() {
     banner
-    read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Username [Lowercase] : \e[0m\e[1;96m\en' user
+    read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Nhập Username [Chữ in thường và không có dấu cách] : \e[0m\e[1;96m\en' user
     echo -e "${W}"
-    read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Password : \e[0m\e[1;96m\en' pass
+    read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Nhập Password : \e[0m\e[1;96m\en' pass
     echo -e "${W}"
     useradd -m -s $(which bash) ${user}
     usermod -aG sudo ${user}
@@ -50,8 +50,8 @@ login() {
 
     clear
     echo
-    echo -e "\n${R} [${W}-${R}]${G} Restart your Termux & Type ${C}ubuntu"${W}
-    echo -e "\n${R} [${W}-${R}]${G} Then Type ${C}sudo bash gui.sh "${W}
+    echo -e "\n${R} [${W}-${R}]${G} Khởi động lại Termux & Nhập ${C}ubuntu"${W}
+    echo -e "\n${R} [${W}-${R}]${G} Hoàn Thành
     echo
 
 }
